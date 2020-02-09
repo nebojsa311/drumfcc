467a7b0bfdaf66e76dbffd8fc22955ef8861b12c
Nebojša Draganić <nebojsa311@gmail.com>
2020-02-09 23:55:00 +0100

    added sound to buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component{
   constructor(props){
     super(props);
   }
 
+  playSound(event){
+    let sound = event.currentTarget.firstChild;
+    sound.play();
+  }
+
+  
   render(){
-    let alarmit = ()=>{alert("Hello motherfucker")}
     return(
       <div id="drum-machine">
         <div id="display">name of sound</div>
-        <div className="drum-pad" id="Heater-1" onKeyDown={alarmit}>Q</div>
-        <div className="drum-pad" id="Heater-2">W</div>
-        <div className="drum-pad" id="Heater-3">E</div>
-        <div className="drum-pad" id="Heater-4">A</div>
-        <div className="drum-pad" id="Clap">S</div>
-        <div className="drum-pad" id='Open-HH'>D</div>
-        <div className="drum-pad" id="Kick-n'-Hat">Z</div>
-        <div className="drum-pad" id="Kick">X</div>
-        <div className="drum-pad" id="Closed-HH">C</div>
+        <div className="drum-pad" id="Heater-1" onClick={this.playSound}>
+          <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="Q" />Q
+        </div>
+        <div className="drum-pad" id="Heater-2"><audio src="#" className="clip" id="W"></audio>W</div>
+        <div className="drum-pad" id="Heater-3"><audio src="#" className="clip" id="E"></audio>E</div>
+        <div className="drum-pad" id="Heater-4"><audio src="#" className="clip" id="A"></audio>A</div>
+        <div className="drum-pad" id="Clap"><audio src="#" className="clip" id="S"></audio>S</div>
+        <div className="drum-pad" id='Open-HH'><audio src="#" className="clip" id="D"></audio>D</div>
+        <div className="drum-pad" id="Kick-n'-Hat"><audio src="#" className="clip" id="Z"></audio>Z</div>
+        <div className="drum-pad" id="Kick"><audio src="#" className="clip" id="X"></audio>X</div>
+        <div className="drum-pad" id="Closed-HH"><audio src="#" className="clip" id="C"></audio>C</div>
       </div>
     )
   }
